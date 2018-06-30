eb77bf03d667e10e59387e0a9305c9a34fc3bbe6
Nurshat <nurshat170k@mail.ru>
2018-06-30 19:12:07 +0600

    init commit

---
+++ js/init.js
@@ -0,0 +1,88 @@
+﻿/*-----------------------------------------------------------------------------------
+/*
+/* Init JS
+/*
+-----------------------------------------------------------------------------------*/
+
+ jQuery(document).ready(function() {
+
+
+/*----------------------------------------------------*/
+/*	Flexslider
+/*----------------------------------------------------*/
+   $('#intro-slider').flexslider({
+      animation: 'fade',
+      controlNav: false,
+   });
+
+/*----------------------------------------------------*/
+/*	gmaps
+------------------------------------------------------*/
+
+   var map;
+
+   // main directions
+   map = new GMaps({
+      el: '#map', lat: 49.802824, lng: 73.090004, zoom: 17, zoomControl : true,
+      zoomControlOpt: { style : 'SMALL', position: 'TOP_LEFT' }, panControl : false, scrollwheel: false
+   });
+
+   // add address markers
+   map.addMarker({ lat: 49.802824, lng: 73.090004, title: 'Eco Tower',
+   infoWindow: { content: '<h5>Наш адрес:Караганда, проспект Нуркена Абдирова, дом 6, Казахстан</h5>' } });
+
+/*----------------------------------------------------*/
+/*	contact form
+------------------------------------------------------*/
+
+   $('form#contactForm button.submit').click(function() {
+
+      $('#image-loader').fadeIn();
+
+      var contactName = $('#contactForm #contactName').val();
+      var contactEmail = $('#contactForm #contactEmail').val();
+      var contactSubject = $('#contactForm #contactSubject').val();
+      var contactMessage = $('#contactForm #contactMessage').val();
+
+      var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
+               '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
+
+      $.ajax({
+
+	      type: "POST",
+	      url: "inc/sendEmail.php",
+	      data: data,
+	      success: function(msg) {
+
+            // Message was sent
+            if (msg == 'OK') {
+               $('#image-loader').fadeOut();
+               $('#message-warning').hide();
+               $('#contactForm').fadeOut();
+               $('#message-success').fadeIn();   
+            }
+            // There was an error
+            else {
+               $('#image-loader').fadeOut();
+               $('#message-warning').html(msg);
+	            $('#message-warning').fadeIn();
+            }
+
+	      }
+
+      });
+
+      return false;
+
+   });
+
+
+});
+
+
+
+
+
+
+
+
